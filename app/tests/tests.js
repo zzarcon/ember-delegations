95209acb03fde20dc8f1439384ff5f2f2bc68558
Hector Zarco <hezarco@gmail.com>
2014-04-04 09:10:04 +0200

    First tests

--- app/tests/tests.js
+++ app/tests/tests.js
@@ -1,6 +1,40 @@
 "use strict";
 Ember.testing = true;
 
-test("Application exists", function() {
-	ok(App instanceof Ember.Application, "Ember application created correctly!");
+var estudies, Son, parent, mySon;
+
+module("Delegated properties", {
+  setup: function() {
+    estudies = ['Primary', 'Secondary', 'University'];
+    Son = Em.Object.extend({
+      name: 'John',
+      age: 24,
+      estudies: estudies
+    });
+    mySon = Son.create();
+    parent = Em.Object.create({
+      delegates: ['son', 'name age estudies'],
+      son: mySon
+    });
+  }
+});
+
+test("Access to delegated properties", function() {
+  var properties = parent.getProperties('name', 'age', 'estudies');
+
+  ok(properties.name === 'John');
+  ok(properties.age === 24);
+  ok(properties.estudies === estudies);
+});
+
+test("Mantain the delegated properties updated", function() {
+  mySon.set('name', 'Daniel');
+  mySon.get('estudies').removeAt(0);
+
+  ok(parent.get('name') === 'Daniel');
+  ok(parent.get('estudies.length') === 2);
+
+  mySon.get('estudies').pushObject('master');
+
+  ok(parent.get('estudies.lastObject') === 'master');
 });
